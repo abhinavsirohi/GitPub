acc3016bea99757cc2b66a610bfafc9cb02a807a
ydjain <yashjainmumbai@gmail.com>
2017-12-01 22:15:26 +0530

    fixed flake8 issues

--- build/lib/samples/most_popular_repo.py
+++ build/lib/samples/most_popular_repo.py
@@ -18,8 +18,12 @@ def sort_repos(repo_list):
         return []
     else:
         pivot = repo_list[0]
-        lesser = sort_repos([repo for repo in repo_list[1:] if repo.stargazers_count < pivot.stargazers_count])
-        greater = sort_repos([repo for repo in repo_list[1:] if repo.stargazers_count >= pivot.stargazers_count])
+        lesser = sort_repos(
+            [repo for repo in repo_list[1:]
+             if repo.stargazers_count < pivot.stargazers_count])
+        greater = sort_repos(
+            [repo for repo in repo_list[1:]
+             if repo.stargazers_count >= pivot.stargazers_count])
         return lesser + [pivot] + greater
 
 
@@ -43,10 +47,12 @@ def main(username='defunkt'):
     profile.get_public_repos()
 
     sorted_repos = reversed(sort_repos(profile.public_repos))
-    print ("%s(%s)'s most popular repositories by stargazers count are:" % (profile.name, profile.username))
+    print("%s(%s)'s most popular repositories "
+          "by stargazers count are:" % (profile.name, profile.username))
     for repo in sorted_repos:
-        print ("%s (%d stars)" % (repo.name, repo.stargazers_count))
+        print("%s (%d stars)" % (repo.name, repo.stargazers_count))
     return sorted_repos
+
 
 if __name__ == '__main__':
     sorted_repos = main()

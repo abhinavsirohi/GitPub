3d13d02a58b6051ba8f633be7f54c5824ef44009
ydjain <yashjainmumbai@gmail.com>
2017-12-01 22:15:26 +0530

    fixed flake8 issues

--- build/lib/tests/test_gitpub.py
+++ build/lib/tests/test_gitpub.py
@@ -1,15 +1,14 @@
-import pytest
 from ..gitpub import gitpub
 
 
 def test_load_gh_profile():
     """
-    Tests gitpub.Profile.load_gh_profile() by loading details of the user `demfier`
-    ------------------------------------------------------------------------------
+    Tests gitpub. Profile.load_gh_profile()
+    by loading details of the user `demfier`
+    --------------------------------------------------------------------------
     Parameters: None
     """
 
-    test_username = 'demfier'
     # define the validation Profile
     correct = gitpub.Profile(username="demfier", name="Gaurav",
                              location="Kharagpur, West Bengal",
@@ -29,17 +28,18 @@ def test_load_gh_profile():
 
 def test_get_public_repos():
     """
-    Tests gitpub.Profile.get_public_repos() by loading public repository details
+    Tests gitpub.Profile.get_public_repos() by
+    loading public repository details
     of the user `demfier`
     ----------------------------------------------------------------------------
     Parameters: None
     """
 
-    test_username = 'demfier'
     # define the validation Profile
     correct = gitpub.Profile(username="demfier", name="Gaurav",
                              location="Kharagpur, West Bengal",
-                             email="sahu.gaurav719@gmail.com", followers_count=10,
+                             email="sahu.gaurav719@gmail.com",
+                             followers_count=10,
                              repos_url="https://api.github.com/users/Demfier/repos",
                              public_repo_count=14)
 
